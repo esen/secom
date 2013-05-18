c2e0ee89c77198e88e8876e2918563a0438de0a2
Esenbek Kydyr uulu <esenbekkd@gmail.com>
2013-05-18 09:32:53 +0600

    Branches added

---
+++ app/controllers/branches_controller.rb
@@ -0,0 +1,72 @@
+class BranchesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @branches = Branch.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @branches }
+    end
+  end
+
+  def show
+    @branch = Branch.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @branch }
+    end
+  end
+
+  def new
+    @branch = Branch.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @branch }
+    end
+  end
+
+  def edit
+    @branch = Branch.find(params[:id])
+  end
+
+  def create
+    @branch = Branch.new(params[:branch])
+
+    respond_to do |format|
+      if @branch.save
+        format.html { redirect_to @branch }
+        format.json { render json: @branch, status: :created, location: @branch }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @branch.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @branch = Branch.find(params[:id])
+
+    respond_to do |format|
+      if @branch.update_attributes(params[:branch])
+        format.html { redirect_to @branch }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @branch.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @branch = Branch.find(params[:id])
+    @branch.destroy
+
+    respond_to do |format|
+      format.html { redirect_to branches_url }
+      format.json { head :no_content }
+    end
+  end
+end
